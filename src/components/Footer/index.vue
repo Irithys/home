ce25c6071190933e58c6feb7a0acf640af0a7376
irithys <thy@irithys.com>
2023-02-14 13:49:56 +0800

    Update index.vue

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,10 +1,7 @@
 <template>
   <footer>
     <div class="power" v-show="!store.playerState">
-      <span
-        >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://imsyy.top">無名</a>
-      </span>
+      
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
         >&nbsp;&amp;&nbsp;Made&nbsp;by&nbsp;<a
@@ -13,10 +10,6 @@
         >
           {{ config.author }}
         </a></span
-      >&nbsp;&amp;
-      <!-- 站点备案 -->
-      <a href="https://beian.miit.gov.cn" target="_blank"
-        >豫ICP备2022018134号-1</a
       >
     </div>
     <div class="lrc" v-show="store.playerState">
